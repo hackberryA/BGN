15b25b0cd50e5761777f2ede374ccaca9139abc3
BuildTools <unconfigured@null.spigotmc.org>
2025-11-13 04:28:04 +0900

    commit

--- src/server/server.ts
+++ src/server/server.ts
@@ -15,9 +15,9 @@ const server = http.createServer(app);
 // WebSocket を統合
 const wsService = new SocketManager(server);
 
-// server.listen(port, () => {
-//     console.log(`HTTP server listening on http://localhost:${port}`);
-// });
+server.listen(port, () => {
+    console.log(`HTTP server listening on http://localhost:${port}`);
+});
 
 // import express from 'express';
 // import path from 'path';
